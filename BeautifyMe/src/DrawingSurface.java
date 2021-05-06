e760233751c90397bebf6ef1e7b580984ac5db2e
Mira Shlimenzon <mshlimenzon924@student.fuhsd.org>
2021-05-06 13:20:42 -0700

    image

--- BeautifyMe/src/DrawingSurface.java
+++ BeautifyMe/src/DrawingSurface.java
@@ -1,3 +1,10 @@
+/**
+ * This class represents a DrawingSurface that contains the Photograph and buttons.
+ * @author Sarah Sabaa
+ * @version 1.0 on 5/6
+ * @since 1.0 
+ */
+
 import java.awt.Button;
 import java.awt.Color;
 import java.awt.FileDialog;
@@ -7,11 +14,9 @@ import java.awt.Label;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JFrame;
 
 public class DrawingSurface implements ActionListener {
-	
 	JFrame fr = new JFrame("Image loading"); //somehow put the Jframe in main! 
 	Label Label1 = new Label("Choose your image");
 	Button Button1 = new Button("select");
@@ -19,6 +24,9 @@ public class DrawingSurface implements ActionListener {
 	Photograph Canvas1;
 	FileDialog fd = new FileDialog(fr, "Open", FileDialog.LOAD);
 
+	/**
+	 *Initializes a Drawing Surface with a 500 by 500 size and creates 2 buttons and an image
+	 */
 	public DrawingSurface() {
 		fr.setSize(500, 500);
 		fr.setLocation(200, 200);
@@ -33,7 +41,10 @@ public class DrawingSurface implements ActionListener {
 		fr.add(Canvas1);
 		fr.show();
 	}
-
+	
+	/**
+	 *Loads an image to the Drawing Surface
+	 */
 	public void imageload() {
 		fd.show();
 		if (fd.getFile() == null) {
@@ -46,7 +57,11 @@ public class DrawingSurface implements ActionListener {
 			Canvas1.repaint();
 		}
 	}
-
+	
+	/**
+	 *Performs an action when the button is pushed 
+	 *@param event An event that has happened when the button is pressed  
+	 */
 	public void actionPerformed(ActionEvent event) {
 		Button b = (Button) event.getSource();
 		if (b == Button1) {
